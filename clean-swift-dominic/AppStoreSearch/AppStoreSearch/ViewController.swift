3673be64c6f3ea45a692e4aac0562c9a0a9b1685
도미닉 <kiljh@me.com>
2025-06-21 09:39:38 +0900

    프로젝트 생성, 스냅킷 설치

---
+++ clean-swift-dominic/AppStoreSearch/AppStoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreSearch
+//
+//  Created by 도미닉 on 6/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
