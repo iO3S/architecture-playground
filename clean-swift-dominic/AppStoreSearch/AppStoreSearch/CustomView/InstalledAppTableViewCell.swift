4190d606979125dafc5aa326e136b22dc5efdb17
도미닉 <kiljh@me.com>
2025-06-30 11:29:28 +0900

    설치된 앱을 보여줄 테이블 뷰 셀 생성

---
+++ clean-swift-dominic/AppStoreSearch/AppStoreSearch/CustomView/InstalledAppTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InstalledAppTableViewCell.swift
+//  AppStoreSearch
+//
+//  Created by 도미닉 on 6/29/25.
+//
+
+import UIKit
+
+class InstalledAppTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
