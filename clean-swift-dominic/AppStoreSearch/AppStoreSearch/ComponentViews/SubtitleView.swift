f9dde3aa74ed4c558e33df0f11242d8571808f5f
도미닉 <kiljh@me.com>
2025-07-05 16:56:37 +0900

    서브타이틀 뷰 - 프로퍼티를 기억하기 위해 변수명으로 프리뷰에 항목 변경

--- clean-swift-dominic/AppStoreSearch/AppStoreSearch/ComponentViews/SubtitleView.swift
+++ clean-swift-dominic/AppStoreSearch/AppStoreSearch/ComponentViews/SubtitleView.swift
@@ -91,7 +91,7 @@ class SubtitleView: UIView {
 
 #Preview {
     SubtitleView().then {
-        let subtitleInfo = SubtitleView.Info(title: "도미닉", subtitle: "개발자")
+        let subtitleInfo = SubtitleView.Info(title: "artistName", subtitle: "개발자")
         $0.info = subtitleInfo
     }
 }
